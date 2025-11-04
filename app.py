b259f2f9794c3942f7be4b4596f1ea841462c6fb
Alex <126032274+alexjohnsonh@users.noreply.github.com>
2025-11-04 17:16:39 +1300

    Update app.py Cache *

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import re
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://ats-environmental.com"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 def mm_to_m(v): 
     return float(v) / 1000.0
