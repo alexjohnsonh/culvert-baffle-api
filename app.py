61c12a398fc6e65d8d3cd6e3cab98966c34b8c7d
Alex <126032274+alexjohnsonh@users.noreply.github.com>
2025-11-04 17:12:49 +1300

    Update app.py cache fix

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ import uuid
 import os
 from matplotlib.patches import Rectangle
 import re
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://ats-environmental.com"}})
 
 def mm_to_m(v): 
     return float(v) / 1000.0
